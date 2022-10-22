a5b90ab67fa8b2d36476a79ab9a27378cca23e7e
JakoError <jakozhou@foxmail.com>
2022-10-22 15:49:23 +0800

    finish regex read

--- main.cpp
+++ main.cpp
@@ -24,14 +24,6 @@ typedef std::istream_iterator<int> in;
 
 string cmd_line = "> ";
 
-string seg = "[\\s\t]+";
-string seg_v = "[\\s\t]*";
-string name_regex = "([A-Za-z0-9]{1,63})";
-string column_regex = "(" + name_regex + seg + "((int)|(string))(" + seg + "primary)?)";
-string value_regex = "((0|[1-9][0-9]*)|(\".{0,256}\"))";
-string cond_regex = "(" + name_regex + seg_v + "[=<>]" + seg_v + value_regex + ")";
-string where_regex = "(" + seg + "where" + seg + cond_regex + ")";
-
 enum type_num {
     int_type = 0, str_type = 1
 };
@@ -92,6 +84,14 @@ void delete_process(const string &cmd);
 
 void insert_process(string &cmd);
 
+string seg = "[\\s\t]+";
+string seg_v = "[\\s\t]*";
+string name_regex = "([a-z]{1,63})";
+string column_regex = "(" + name_regex + seg + "((int)|(string))(" + seg + "primary)?)";
+string value_regex = "((0|[1-9][0-9]*)|(\".{0,256}\"))";
+string cond_regex = "(" + name_regex + seg_v + "[=<>]" + seg_v + value_regex + ")";
+string where_regex = "(" + seg + "where" + seg + cond_regex + ")";
+
 int main() {
     regex re_create_db("create" + seg + "database" + seg + name_regex);
     regex re_drop_db("drop" + seg + "database" + seg + name_regex);
@@ -99,7 +99,7 @@ int main() {
     regex re_create_tb(
             "create" + seg + "table" + seg + name_regex + seg_v + "\\(" + seg_v + column_regex + "(" + seg_v + "," +
             seg_v + column_regex + ")+" + seg_v + "\\)");
-    regex re_drop_tb("drop" + seg + "table" + seg + "[A-Za-z0-9]{1,63}");
+    regex re_drop_tb("drop" + seg + "table" + seg + name_regex);
     regex re_select("select" + seg + "(" + name_regex + "|[*])" + seg + "from" + seg + name_regex + where_regex + "?");
     regex re_delete("delete" + seg + name_regex + where_regex + "?");
     regex re_insert("insert" + seg + name_regex + seg + "values" + seg_v + "\\(" + seg_v +
@@ -111,6 +111,7 @@ int main() {
         string cmd;
         getline(cin, cmd);
         trim(cmd);
+        cout << cmd << ":" << cmd.length() << endl;
         try {
             if (regex_match(cmd, re_create_db)) {
                 create_db_process(cmd);
@@ -203,53 +204,74 @@ void drop_tb_process(const string &cmd) {
 }
 
 void select_process(const string &cmd) {
-    auto strs = split_by_space(cmd);
-    if (strs.size() == 4) {
-        select_value(strs[1], strs[3]);
-    } else if (strs.size() == 8) {
-        auto &value_str = strs[7];
-        if (is_str_type(value_str)) {
-            boost::algorithm::trim_if(value_str, boost::algorithm::is_any_of("\""));
-            select_value(strs[1], strs[3], strs[5], cond_op_map[strs[6]], value_str);
-        } else {
-            select_value(strs[1], strs[3], strs[5], cond_op_map[strs[6]],
-                         boost::lexical_cast<int>(value_str));
-        }
+    auto value_idx = cmd.find_first_of('\"');
+    string head_str = cmd.substr(0, value_idx);
+    string value_str;
+    if (value_idx != string::npos)
+        value_str = cmd.substr(value_idx);
+    trim(head_str);
+    trim(value_str);
+    auto head_strs = split_by_space(head_str);
+    if (head_strs.size() == 4) {
+        select_value(head_strs[1], head_strs[3]);
+    } else if (head_strs.size() == 7) {
+        select_value(head_strs[1], head_strs[3], head_strs[5], cond_op_map[head_strs[6]], value_str);
+    } else if (head_strs.size() == 8) {
+        select_value(head_strs[1], head_strs[3], head_strs[5], cond_op_map[head_strs[6]],
+                     boost::lexical_cast<int>(head_strs[7]));
     } else {
         BOOST_THROW_EXCEPTION(std::runtime_error("syntax match select value error"));
     }
 }
 
 void delete_process(const string &cmd) {
-    auto strs = split_by_space(cmd);
-    if (strs.size() == 2)
-        delete_value(strs[1]);
-    else if (strs.size() == 6) {
-        auto &value_str = strs[5];
-        if (is_str_type(value_str)) {
-            boost::algorithm::trim_if(value_str, boost::algorithm::is_any_of("\""));
-            delete_value(strs[1], strs[3], cond_op_map[strs[4]], value_str);
-        } else {
-            delete_value(strs[1], strs[3], cond_op_map[strs[4]], boost::lexical_cast<int>(value_str));
-        }
+    auto value_idx = cmd.find_first_of('\"');
+    string head_str = cmd.substr(0, value_idx);
+    string value_str;
+    if (value_idx != string::npos)
+        value_str = cmd.substr(value_idx);
+    trim(head_str);
+    trim(value_str);
+    auto head_strs = split_by_space(head_str);
+
+    if (head_strs.size() == 2)
+        delete_value(head_strs[1]);
+    else if (head_strs.size() == 5) {
+        delete_value(head_strs[1], head_strs[3], cond_op_map[head_strs[4]], value_str);
+    } else if (head_strs.size() == 6) {
+        delete_value(head_strs[1], head_strs[3], cond_op_map[head_strs[4]], boost::lexical_cast<int>(head_strs[5]));
     } else {
         BOOST_THROW_EXCEPTION(std::runtime_error("syntax match delete table error"));
     }
 }
 
 void insert_process(string &cmd) {
+    //insert person values (1234, "123aaa", "aaa,bbb", " aaabbb \\" )
     std::vector<string> strs;
     auto brac_split_idx = cmd.find_first_of('(');
-    string head_str = cmd.substr(0,brac_split_idx);
-    string trail_str = cmd.substr(brac_split_idx+1);
-
+    string head_str = cmd.substr(0, brac_split_idx);
+    string trail_str = cmd.substr(brac_split_idx + 1);
 
     auto head_strs = split_by_space(head_str);
     auto &tb_name = head_strs[1];
 
-    std::vector<string> value_strs;
-    for (auto iter_r = trail_str.begin(),iter_l=iter_r; iter_r != trail_str.end()-1; ++iter_r) {
-
+    vector<string> value_strs;
+    bool in_str = false;
+    for (int l = 0, r = 1; r < trail_str.size(); ++r) {
+        if (in_str) {
+            if (trail_str[r] == '\"')
+                in_str = false;
+            continue;
+        }
+        if (trail_str[r] == '\"') {
+            in_str = true;
+            continue;
+        }
+        if (trail_str[r] == ',' || trail_str[r] == ')') {
+            //分隔符
+            value_strs.push_back(trail_str.substr(l, r - l));
+            l = r + 1;
+        }
     }
     for (auto &value_str: value_strs) {
         trim(value_str);
@@ -264,51 +286,51 @@ vector<string> split_by_space(const string &str) {
 }
 
 void create_database(string &db_name) {
-    cout<<"create db:"<<db_name<<endl;
+    cout << "create db:" << db_name << endl;
 }
 
 void drop_database(string &db_name) {
-    cout<<"drop db:"<<db_name<<endl;
+    cout << "drop db:" << db_name << endl;
 }
 
 void use_database(string &db_name) {
-    cout<<"use db:"<<db_name<<endl;
+    cout << "use db:" << db_name << endl;
 }
 
 void create_table(string &tb_name, vector<string> &column_names, vector<type_num> &column_types, int primary_index) {
-    cout<<"create tb:"<<tb_name<<endl;
+    cout << "create tb:" << tb_name << endl;
     for (int i = 0; i < column_names.size(); ++i) {
-        cout<<i<<"\t:"<<column_names[i]<<" "<<column_types[i]<<endl;
+        cout << i << "\t:" << column_names[i] << " " << column_types[i] << endl;
     }
-    cout<<"primary:"<<primary_index<<endl;
+    cout << "primary:" << primary_index << endl;
 }
 
-void drop_table(string &tb_name){
-    cout<<"drop tb:"<<tb_name<<endl;
+void drop_table(string &tb_name) {
+    cout << "drop tb:" << tb_name << endl;
 }
 
-void select_value(string &column, string &tb_name){
-    cout<<"select val:"<<column<<"on tb:"<<tb_name<<endl;
-}
-
-template<typename Type>
-void select_value(string &column, string &tb_name, string &cond_column, cond_op_num cond_op, Type cond_value){
-    cout<<"select val:"<<column<<" on tb:"<<tb_name<<cond_column<<cond_op<<cond_value<<endl;
-}
-
-void delete_value(string &tb_name){
-    cout<<"delete tb:"<<tb_name<<endl;
+void select_value(string &column, string &tb_name) {
+    cout << "select val:" << column << "on tb:" << tb_name << endl;
 }
 
 template<typename Type>
-void delete_value(string &tb_name, string &cond_column, cond_op_num cond_op, Type cond_value){
-    cout<<"delete tb:"<<tb_name<<cond_column<<cond_op<<cond_value<<endl;
+void select_value(string &column, string &tb_name, string &cond_column, cond_op_num cond_op, Type cond_value) {
+    cout << "select val:" << column << " on tb:" << tb_name << cond_column << cond_op << cond_value << endl;
+}
+
+void delete_value(string &tb_name) {
+    cout << "delete tb:" << tb_name << endl;
+}
+
+template<typename Type>
+void delete_value(string &tb_name, string &cond_column, cond_op_num cond_op, Type cond_value) {
+    cout << "delete tb:" << tb_name << cond_column << cond_op << cond_value << endl;
 
 }
 
-void insert_value(string &tb_name, vector<string> &values){
-    cout<<"insert val on table:"<<tb_name<<endl;
-    for (auto &val:values) {
-        cout<<val<<endl;
+void insert_value(string &tb_name, vector<string> &values) {
+    cout << "insert val on table:" << tb_name << endl;
+    for (auto &val: values) {
+        cout << val << endl;
     }
 }
